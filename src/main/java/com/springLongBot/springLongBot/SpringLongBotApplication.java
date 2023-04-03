7962a0b3e8e23342aad49df572647f5a397b6f58
Vladyslav Pustovalov <pustovalov.v.y@gmail.com>
2023-04-03 20:33:00 +0300

    init commit

---
+++ src/main/java/com/springLongBot/springLongBot/SpringLongBotApplication.java
@@ -0,0 +1,13 @@
+package com.springLongBot.springLongBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLongBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLongBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springLongBot/springLongBot/SpringLongBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springLongBot.springLongBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLongBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
