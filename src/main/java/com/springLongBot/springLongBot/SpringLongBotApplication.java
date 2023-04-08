e3fa4d3ec3ebb32da8953dbcd79950d6eeb1c12d
Vladyslav Pustovalov <pustovalov.v.y@gmail.com>
2023-04-08 16:28:31 +0300

    init commit

--- src/main/java/com/springLongBot/springLongBot/SpringLongBotApplication.java
+++ src/main/java/com/springLongBot/springLongBot/SpringLongBotApplication.java
@@ -1,5 +1,6 @@
 package com.springLongBot.springLongBot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,4 @@ public class SpringLongBotApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLongBotApplication.class, args);
 	}
-
 }
