9327d68a05bf90c4af0133f171cad3dacbc1a6e0
Vladyslav Pustovalov <pustovalov.v.y@gmail.com>
2023-04-08 17:14:39 +0300

    changing to 5.7 version

--- src/main/java/com/springLongBot/springLongBot/BotInitializer.java
+++ src/main/java/com/springLongBot/springLongBot/BotInitializer.java
@@ -1,11 +1,9 @@
 package com.springLongBot.springLongBot;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -14,19 +12,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 @RequiredArgsConstructor
 public class BotInitializer {
 
-    private final String botToken = System.getenv("botToken");
-
-    @Bean
-    private String getToken() {
-        return botToken;
-    }
-
-    @Bean
-    DefaultBotOptions options() {
-        return new DefaultBotOptions();
-    }
-
-     SpringLongBot bot = new SpringLongBot(options(), getToken());
+    SpringLongBot bot = new SpringLongBot();
 
     @EventListener({ContextRefreshedEvent.class})
     public void init() {
--- src/main/java/com/springLongBot/springLongBot/SpringLongBot.java
+++ src/main/java/com/springLongBot/springLongBot/SpringLongBot.java
@@ -1,7 +1,6 @@
 package com.springLongBot.springLongBot;
 
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -11,14 +10,19 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class SpringLongBot extends TelegramLongPollingBot {
 
     private final String botName = System.getenv("botName");
+    private final String botToken = System.getenv("botToken");
 
-    SpringLongBot (DefaultBotOptions options, String botToken) {
-        super(options, botToken);
+    SpringLongBot () {
     }
 
     @Override
     public String getBotUsername() {
         return botName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
     }
 
     @Override
