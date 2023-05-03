f8b35ce3aa15d2e7415f9e633711f0f14ccb3c21
Vladyslav Pustovalov <pustovalov.v.y@gmail.com>
2023-05-03 20:38:34 +0300

    controller test

--- src/main/java/com/sprint_long_bot/controller/BotController.java
+++ src/main/java/com/sprint_long_bot/controller/BotController.java
@@ -1,12 +1,14 @@
 package com.sprint_long_bot.controller;
 
 import com.sprint_long_bot.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class BotController {
 
     public BotController() {
@@ -14,11 +16,13 @@ public class BotController {
 
     @GetMapping(path = "/hello")
     public String getHelloWorldFromBot() {
+        log.info("GET works");
         return "Hello World from Bot!";
     }
 
     @PostMapping("/hello")
     public String takeUpdateFromRest(@RequestBody String update) {
+        log.info("POST works, '"+update+"' is took via Postman");
         return "update is took"+update;
     }
 }
